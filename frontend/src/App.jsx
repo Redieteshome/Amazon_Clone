b9eeaa0be26a3f5df8744ec1f6e62cc895ff850b
Rediet Teshome Tadesse <redieteshome78@gmail.com>
2025-12-05 18:04:43 -0800

    CarouselEfecct component and its module Css completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
+import CarouselEffect from './Components/CarouselEffect/CarouselEffect'
 
 function App() {
  
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <CarouselEffect />
     </>
   )
 }
