9ae34e21bc7f6dd002e63acf95cf0285cd4137ac
Rediet Teshome Tadesse <redieteshome78@gmail.com>
2025-12-06 14:27:32 -0800

    Products component and its Modular Css completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './Components/Header/Header'
 import CarouselEffect from './Components/CarouselEffect/CarouselEffect'
 import Category from './Components/Catgeory/Category'
+import Product from './Components/Product/Product'
 
 function App() {
  
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <CarouselEffect />
       <Category/>
+      <Product />
     </>
   )
 }
