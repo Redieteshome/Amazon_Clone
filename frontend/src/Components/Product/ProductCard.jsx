02a26a9ff18d8ed80c55c494afcd86cf7068f306
Rediet Teshome Tadesse <redieteshome78@gmail.com>
2025-12-06 17:08:28 -0800

    Detailed Page Routing of single product completed

--- frontend/src/Components/Product/ProductCard.jsx
+++ frontend/src/Components/Product/ProductCard.jsx
@@ -2,32 +2,35 @@ import React from 'react'
 import Rating from '@mui/material/Rating'
 import CurrencyFormat from '../CurrencyFormat/CurrencyFormat'
 import styles from './product.module.css'
+import { Link } from 'react-router-dom'
+
+
 function ProductCard({product}) {
     const {image, title, id, rating, price} = product;
   return (
     <div className={`${styles.card_container}`}>
-        <a href="">
-            <img src={image} alt="" />
-        </a>
+      <Link to={`/products/${id}`}>
+        <img src={image} alt="" />
+      </Link>
+
+      <div>
+        <h3>{title}</h3>
+        <div className={styles.rating}>
+          {/* rating */}
+          <Rating value={rating?.rate || 0} precision={0.1} />
+          {/* rating counter */}
+          <small>{rating?.count || 0}</small>
+        </div>
 
         <div>
-            <h3>{title}</h3>
-            <div className={styles.rating}>
-                {/* rating */}
-                <Rating value={rating.rate} precision={0.1}/>
-                {/* rating counter */}
-                <small>{rating.count}</small>
-            </div>
-
-            <div>
-               {/* price  */}
-               <CurrencyFormat amount={price}/>
-            </div>
-
-            <button className={styles.button}>add to cart</button>
+          {/* price  */}
+          <CurrencyFormat amount={price} />
         </div>
+
+        <button className={styles.button}>add to cart</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ProductCard
--- frontend/src/Pages/ProductDetail/ProductDetail.jsx
+++ frontend/src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
 import styles from "./productdetail.module.css";
 import Layout from '../../Components/Layout/Layout';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import ProductCard from '../../Components/Product/ProductCard';
+import { productUrl } from '../../Api/endpoint';
+
 function ProductDetail() {
-  return (
+    const {productId} = useParams() 
+    const [singleProduct,setSingleProduct] =useState({})
+    
+    useEffect(() => {
+        axios.get(`${productUrl}/products/${productId}`)
+        .then((res) => {
+          setSingleProduct(res.data)
+        }).catch((err) => {console.log(err)})
+    }, [])
+  
+    return (
     <Layout>
-      <div>ProductDetail</div>
+      <ProductCard product ={singleProduct}/>
     </Layout>
   );
 }
