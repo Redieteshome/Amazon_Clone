2b9010b3a0ef5a38fe2640032550417a121726e3
Rediet Teshome Tadesse <redieteshome78@gmail.com>
2025-12-20 08:47:26 -0800

    Update backend API URL

--- frontend/src/Api/axios.js
+++ frontend/src/Api/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    // local instance of firebase functions
-    // baseURL:"http://127.0.0.1:5001/clone-5e17d/us-central1/api"
+  // local instance of firebase functions
+  // baseURL:"http://127.0.0.1:5001/clone-5e17d/us-central1/api"
 
-    // deployed version of amazon server on render.com
-    baseURL: "https://amazon-clone-backend-p3jr.onrender.com"
-})
+  // deployed version of amazon server on render.com
+  baseURL: "https://amazon-clone-fui9.onrender.com",
+});
 
 export {instance}
 
